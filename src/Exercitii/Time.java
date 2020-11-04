0774981dd1696bcc6b979b00f0581058948195f1
Parpala Andreea <parpalaandreea@gmail.com>
2020-11-04 16:56:14 +0200

    Add zero to clase Time

--- src/Exercitii/Time.java
+++ src/Exercitii/Time.java
@@ -13,18 +13,30 @@ public class Time {
             hour = (int) minute / 60;
             remainingMinute = (int) minute % 60;
 
-            return hour + "hh " + remainingMinute + "mm " + second + "ss";
+            String hourString = hour + "hh ";
+            if (hour < 10) {
+                hourString = "0" + hourString;
+            }
+            String minuteString = remainingMinute + "mm ";
+            if (remainingMinute < 10) {
+                minuteString = "0" + minuteString;
+            }
+            String secondString = remainingSecond + "ss ";
+            if (remainingSecond < 10) {
+                secondString = "0" + secondString;
+            }
+//            return hour + "hh " + remainingMinute + "mm " + second + "ss";
+            return hourString +" "+ minuteString +" "+ secondString;
         } else
             return "Invalid value";
     }
 
     public static String getStringTime(long second) {
         if (second >= 60) {
-            minute=second/60;
-            remainingSecond=second%60;
-            return getStringTime(minute,remainingSecond);
-        }
-        else
+            minute = second / 60;
+            remainingSecond = second % 60;
+            return getStringTime(minute, remainingSecond);
+        } else
             return "Invalid value";
     }
 }
